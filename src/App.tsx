25e53102a57cbf57030d115891d691241f5fd780
viniAmorim <amorimm.viniciuss@gmail.com>
2023-09-30 12:35:32 -0300

    fetch data from users endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,36 @@
+import { useState } from 'react'
+import { useQuery } from 'react-query'
+
+import Linearprogress from '@material-ui/core/LinearProgress'
+
+import { Wrapper } from './App.styles'
+
+export type UserItemType = {
+  id: number;
+  name: string;
+  email: string;
+  profile: string;
+  phone: string;
+  age: number;
+}
+
+const getUsers = async (): Promise<UserItemType[]> =>
+  await(await fetch('http://localhost:5000/users')).json()
+
 const App = () => {
+  const { data, isLoading, error } = useQuery<UserItemType[]>(
+    'users', 
+    getUsers
+  )
+
+  const getTotalUsers = () => null
+
+  const handleAddUser = () => null
+
+  if(isLoading) return <Linearprogress />
+  if(error) return <div>Something went wrong ...</div>
+
+  console.log(data)
   return (
     <div>
       CRUD SUPERA
