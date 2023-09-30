092b81d5cb0e8b8ce78ab74d4a6b5f23bff8af35
viniAmorim <amorimm.viniciuss@gmail.com>
2023-09-30 13:10:57 -0300

    fetch users on UsersTable and adding table with pagination

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { useQuery } from 'react-query'
 import Linearprogress from '@material-ui/core/LinearProgress'
 
 import { Wrapper } from './App.styles'
+import UsersTable from './Table/Table'
+import PageWithPagination from './TableWithPagination/PageWithPagination'
 
 export type UserItemType = {
   id: number;
@@ -14,27 +16,11 @@ export type UserItemType = {
   age: number;
 }
 
-const getUsers = async (): Promise<UserItemType[]> =>
-  await(await fetch('http://localhost:5000/users')).json()
-
 const App = () => {
-  const { data, isLoading, error } = useQuery<UserItemType[]>(
-    'users', 
-    getUsers
-  )
-
-  const getTotalUsers = () => null
-
-  const handleAddUser = () => null
-
-  if(isLoading) return <Linearprogress />
-  if(error) return <div>Something went wrong ...</div>
-
-  console.log(data)
   return (
-    <div>
-      CRUD SUPERA
-    </div>
+    <>
+      <PageWithPagination />
+    </>
   );
 }
 
---
+++ src/Table/Table.tsx
@@ -0,0 +1,73 @@
+import * as React from 'react';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import { LinearProgress } from '@material-ui/core';
+import { useQuery } from 'react-query';
+
+export type UserItemType = {
+  id: number;
+  name: string;
+  email: string;
+  profile: string;
+  phone: string;
+  age: number;
+}
+
+const getUsers = async (): Promise<UserItemType[]> =>
+  await(await fetch('http://localhost:5000/users')).json()
+
+const UsersTable = () => {
+  const { data, isLoading, error } = useQuery<UserItemType[]>(
+    'users', 
+    getUsers
+  )
+
+  const getTotalUsers = () => null
+
+  const handleAddUser = () => null
+
+  if(isLoading) return <LinearProgress />
+  if(error) return <div>Something went wrong ...</div>
+
+  console.log(data)
+
+  return (
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>ID</TableCell>
+            <TableCell align="right">Name</TableCell>
+            <TableCell align="right">Email</TableCell>
+            <TableCell align="right">Profile</TableCell>
+            <TableCell align="right">Phone</TableCell>
+            <TableCell align="right">Age</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data?.map((user) => (
+            <TableRow
+              key={user.name}
+            >
+              <TableCell component="th" scope="row">
+                {user.id}
+              </TableCell>
+              <TableCell align="right">{user.name}</TableCell>
+              <TableCell align="right">{user.email}</TableCell>
+              <TableCell align="right">{user.profile}</TableCell>
+              <TableCell align="right">{user.phone}</TableCell>
+              <TableCell align="right">{user.age}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
+
+export default UsersTable
